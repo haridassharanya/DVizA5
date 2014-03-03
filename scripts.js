742a34c341cac3d4e7bab0520a8fc2abc76c8f2f
Sharanya Haridas <sharanya0211@gmail.com>
2014-03-02 18:58:53 -0800

    with document ready and pageLoaded function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
 /**
  * @author Sharanya Haridas
  */
-console.log("js loaded");
 
+/*Project Steps:
+ * 1. Set up document ready
+ * 2. Load Google Viz library,Google charts package
+ * 3.Load data
+ * 4.Render chart
+ */
+
+/*page loaded function, responds to document ready*/
+
+function pageLoaded(){
+	console.log("page done!");
+}
+
+
+/*document ready function*/
+
+$document.ready(pageLoaded);
