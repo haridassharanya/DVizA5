399b90547cb3210cc3787b3001445912294c2bbb
Sharanya Haridas <sharanya0211@gmail.com>
2014-03-02 20:37:46 -0800

    data array showing in firefox

--- scripts.js
+++ scripts.js
@@ -13,12 +13,34 @@
 
 function dataLoaded(unempData){
 	
-	console.log(unempData);
+	
+	var myObsArray =unempData.observations;
+	console.log(myObsArray);
+	var myDataList = [];
+	
+	
+	//converting json data to an array of arrays 
+	//using a for loop
+	//this is to create my vizualization
+	
+	for(var i=0; i<myObsArray.length; i++){
+		
+		var myObsObj = myObsArray[i];
+		var myDataArray = [myObsObj.date, myObsObj.value];
+		myDataList.push(myDataArray);
+		
+	}
+	
+console.log(myDataList);
+	
 }
+
 
 function googleVizloaded(){
 	
 	console.log("google viz loaded!");
+	
+	//get function for loading data
 	
 	$.get("UEMP270V_data.json", dataLoaded, "json");
 }
